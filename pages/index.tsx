bbcafde137478912e46b1da852686d481d6fb1c3
Jakub Musik <jakubmusik@gmail.com>
2021-07-30 22:15:47 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import React from 'react'
+import { RenderALot } from '../components/RenderALot'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <div className={styles.content}>
+
+      </div>
+      <div id="copyme">
+        <RenderALot />
+      </div>
+    </div>
+  )
+}
